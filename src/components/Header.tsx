6c8369e99a4addff0c89b99683689baea5588bc6
travis <travis.x.wong@gmail.com>
2022-03-16 11:10:07 +0800

    使用styled-components修改Header组件

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,53 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
+import LogoUrl from './logo.svg'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+  background: #343a40;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+
+  > img {
+    height: 30px;
+  }
+
+  > nav > a {
+    text-decoration: none;
+    color: #9a9787;
+    margin-left: 16px;
+
+    &:hover {
+      color: #cccdb8;
+    }
+
+    &.active {
+      position: relative;
+    }
+
+    &.active::after {
+      content: '';
+      display: block;
+      height: 2px;
+      width: 100%;
+      position: absolute;
+      left: 0;
+      top: 28px;
+      background: #2280ff;
+
+    }
+  }
+`
 
 export const Header:React.FC = () => {
   return (
-    <>
-    <div>
+    <StyledHeader>
+      <img src={LogoUrl} alt="logo" />
       <nav>
-        <Link to="/">首页</Link>
-        <Link to="/history">上传历史</Link>
-        <Link to="/about">关于</Link>
+        <NavLink to="/">首页</NavLink>
+        <NavLink to="/history">上传历史</NavLink>
+        <NavLink to="/about">关于</NavLink>
       </nav>
-    </div>
-    </>
+    </StyledHeader>
   )
 }
