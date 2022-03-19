547d6d49601a5049b0f5ba551e6c6de5ba9cda2d
travis <travis.x.wong@gmail.com>
2022-03-19 20:57:31 +0800

    添加注册表单

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,9 +1,84 @@
+import { Form, Input, Button } from 'antd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 30px auto;
+  padding: 20px 50px;
+  border-radius: 4px;
+  box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, .2);
+
+  .ant-form-item-control-input-content {
+    display: flex;
+    justify-content: center;
+  }
+
+  > h2 {
+    text-align: center;
+    padding: 8px 0;
+  }
+`
+
 const Register: React.FC = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
-    <>
-      <div>Register</div>
-    </>
-  )
+    <Wrapper>
+      <h2>欢迎</h2>
+    <Form
+      name="basic"
+      labelCol={{ span: 4 }}
+      wrapperCol={{ span: 20 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="用户名"
+        name="username"
+        rules={[{ required: true, message: '请输入用户名!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="密码"
+        name="password"
+        rules={[{ required: true, message: '请输入密码!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="确认密码"
+        name="Confirm password"
+        rules={[{ required: true, message: '请输入确认密码!' }, ({ getFieldValue }) => ({
+          validator(_, value) {
+            if (!value || getFieldValue('password') === value) {
+              return Promise.resolve();
+            }
+            return Promise.reject(new Error('两次密码输入不一致!'));
+          },
+        }),]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ span: 24 }}>
+        <Button type="primary" htmlType="submit">
+          注册
+        </Button>
+      </Form.Item>
+    </Form>
+    </Wrapper>
+  );
 }
 
 export default Register
