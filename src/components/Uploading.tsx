b4ead19252805dc2d33038f846fae4bf11676b58
travis <travis.x.wong@gmail.com>
2022-03-23 23:20:02 +0800

    使用antd Uploader组件实现图片上传

--- src/components/Uploading.tsx
+++ src/components/Uploading.tsx
@@ -1,24 +1,44 @@
 import { MutableRefObject, useRef } from 'react'
 import { useStore } from '../store'
+import { Upload } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+
+const { Dragger } = Upload;
 
 const Uploading = () => {
-  const inputRef = useRef() as MutableRefObject<HTMLInputElement>
   const { ImageStore } = useStore()
-  const handleUpload = () => {
-    if (inputRef.current.files && inputRef.current.files.length > 0) {
-      ImageStore.setFile(inputRef.current.files[0])
-      ImageStore.setFilename(inputRef.current.files[0].name)
+
+  const props = {
+    showUploadList: false,
+    beforeUpload: (file:any) => {
+      ImageStore.setFile(file)
+      ImageStore.setFilename(file.name)
       ImageStore.upload()
         .then((file) => console.dir(file))
         .catch((err) => {
           console.error(err)
         })
-    }
+      return false;
+    },
   }
+
   return (
     <>
       <h2>文件上传</h2>
-      <input type="file" ref={inputRef} onChange={handleUpload} />
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Click or drag file to this area to upload</p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+          band files
+        </p>
+      </Dragger>
+      <div>
+        <h2>保存地址</h2>
+        {ImageStore.serverFile ? ImageStore.serverFile.attributes.url.attributes.url : ''}
+      </div>
     </>
   )
 }
