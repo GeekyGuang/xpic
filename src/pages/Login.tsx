1e2f876ec6fbf9ffb0ba20ab2169a78ff0c4b2a3
TravisWongX <travis.x.wong@gmail.com>
2022-03-25 14:46:32 +0800

    提示登录才能使用

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,27 +3,32 @@ import { Link, useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { useStore } from '../store'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import { useEffect, useState } from 'react'
 
 const Wrapper = styled.div`
   max-width: 270px;
   margin: 50px auto;
-  padding: 20px 20px;
-  border-radius: 4px;
-  box-shadow: 0px 0px 5px 0 rgba(0, 0, 0, 0.2);
 
-  @media (min-width: 576px) {
-    max-width: 270px;
+  > .notes {
+    font-size: 16px;
+    line-height: 16px;
+    padding: 10px;
+    border-radius: 4px;
+    background-color: #c3f5cf;
+    border: 1px solid #7ee281;
+    color: #155724;
+    margin-bottom: 20px;
+  }
+
+  > div {
     padding: 20px 20px;
-  }
+    border-radius: 4px;
+    box-shadow: 0px 0px 5px 0 rgba(0, 0, 0, 0.2);
 
-  @media (min-width: 768px) {
-    max-width: 360px;
-    padding: 20px 30px;
-  }
-
-  @media (min-width: 992px) {
-    max-width: 480px;
-    padding: 20px 50px;
+    > h2 {
+      text-align: center;
+      padding: 8px 0;
+    }
   }
 
   .ant-form-item-control-input-content {
@@ -31,15 +36,38 @@ const Wrapper = styled.div`
     justify-content: center;
   }
 
-  > h2 {
-    text-align: center;
-    padding: 8px 0;
+  @media (min-width: 576px) {
+    max-width: 270px;
+    > div {
+      padding: 20px 20px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    max-width: 360px;
+    > div {
+      padding: 20px 30px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    max-width: 480px;
+    > div {
+      padding: 20px 50px;
+    }
   }
 `
 
 const Login: React.FC = () => {
   const { AuthStore } = useStore()
   const navigate = useNavigate()
+  const [show, setShow] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false)
+    }, 3000)
+  }, [])
 
   const onFinish = (values: any) => {
     console.log('Success:', values)
@@ -65,42 +93,45 @@ const Login: React.FC = () => {
 
   return (
     <Wrapper>
-      <h2>用户登录</h2>
-      <Form
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item
-          name="username"
-          rules={[{ required: true, message: '请输入用户名!' }]}
+      {show ? <p className="notes">请先登录才能使用哦~</p> : ''}
+      <div>
+        <h2>用户登录</h2>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="用户名"
-          />
-        </Form.Item>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: '请输入用户名!' }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="用户名"
+            />
+          </Form.Item>
 
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: '请输入密码!' }]}
-        >
-          <Input.Password
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            placeholder="密码"
-          />
-        </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: '请输入密码!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              placeholder="密码"
+            />
+          </Form.Item>
 
-        <Form.Item wrapperCol={{ span: 24 }}>
-          <Button type="primary" htmlType="submit">
-            登录
-          </Button>
-        </Form.Item>
-      </Form>
-      <div style={{ textAlign: 'center' }}>
-        还没有账号？去<Link to="/register">注册</Link>
+          <Form.Item wrapperCol={{ span: 24 }}>
+            <Button type="primary" htmlType="submit">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+        <div style={{ textAlign: 'center' }}>
+          还没有账号？去<Link to="/register">注册</Link>
+        </div>
       </div>
     </Wrapper>
   )
