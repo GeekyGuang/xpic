dfb8d5dc3eb7fd7f91e291e077575d7cc4573e5d
TravisWongX <travis.x.wong@gmail.com>
2022-03-25 17:50:46 +0800

    main部分完成响应式布局

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from './components/Header'
 import Loading from './components/Loading'
 import './App.css'
 import { useStore } from './store'
+import styled from 'styled-components'
 
 const Home = lazy(() => import('./pages/Home'))
 const About = lazy(() => import('./pages/About'))
@@ -18,34 +19,53 @@ const PrivateRoute = ({ children }) => {
   return currentUser ? children : <Navigate to="/login" />
 }
 
+const MainWrapper = styled.div`
+  width: 100%;
+  margin: 20px auto;
+
+  @media (min-width: 980px) {
+    max-width: 92%;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 78%;
+  }
+
+  @media (min-width: 1470px) {
+    max-width: 72%;
+  }
+`
+
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Suspense fallback={<Loading />}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Home />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/about" element={<About />} />
-            <Route
-              path="/history"
-              element={
-                <PrivateRoute>
-                  <History />
-                </PrivateRoute>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </Suspense>
+        <MainWrapper>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Home />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/about" element={<About />} />
+              <Route
+                path="/history"
+                element={
+                  <PrivateRoute>
+                    <History />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </Suspense>
+        </MainWrapper>
       </main>
       <Footer />
     </>
