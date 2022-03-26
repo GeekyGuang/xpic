e2f52c5207f96cc3aca51f978a63543c1cdc7082
travis <travis.x.wong@gmail.com>
2022-03-26 23:27:58 +0800

    添加上传结果样式

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -2,8 +2,51 @@ import { useStore } from '../store'
 import { Upload } from 'antd'
 import { InboxOutlined } from '@ant-design/icons'
 import { observer } from 'mobx-react'
+import styled from 'styled-components'
 
 const { Dragger } = Upload
+
+const Result = styled.div`
+  margin-top: 20px;
+  
+  > dl {
+    border: 1px dashed #6ebcfc;
+    background: #fafafa;
+    padding: 20px;
+
+    .filename {
+      border: 1px solid #999;
+      width: 100%;
+      padding: 4px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    > dt {
+      padding-bottom: 6px;
+    }
+
+    a {
+      display: inline-block;
+      width: 100%;
+      border: 1px solid #999;
+      padding: 4px;
+      white-space: wrap;
+      word-wrap:break-word;
+      background: white;
+    }
+
+    img {
+      max-height: 280px;
+      max-width: 100%;
+      object-fit: contain;
+    }
+  }
+
+
+`
 
 const Uploader = observer(() => {
   const { ImageStore } = useStore()
@@ -24,23 +67,35 @@ const Uploader = observer(() => {
 
   return (
     <>
-      <h2>文件上传</h2>
+      <h2>上传图片</h2>
       <Dragger {...props}>
         <p className="ant-upload-drag-icon">
           <InboxOutlined />
         </p>
         <p className="ant-upload-text">
-          Click or drag file to this area to upload
+          点击或拖拽图片到这里上传
         </p>
         <p className="ant-upload-hint">
-          Support for a single or bulk upload. Strictly prohibit from uploading
-          company data or other band files
+          严禁上传色情/暴力/侵犯版权等违规图片
         </p>
       </Dragger>
       <div>
-        <h2>保存地址</h2>
         {ImageStore.serverFile
-          ? ImageStore.serverFile.attributes.url.attributes.url
+          ? (<Result>
+             <h2>上传结果</h2>
+             <dl>
+               <dt>线上地址</dt>
+               <dd><a target="_blank" href={ImageStore.serverFile.attributes.url.attributes.url} rel="noreferrer">{ImageStore.serverFile.attributes.url.attributes.url}</a></dd>
+               <dt>文件名</dt>
+               <dd><input className="filename" type="text" value={ImageStore.filename} readOnly /></dd>
+               <dt>图片预览</dt>
+               <dd>
+                 <img src={ImageStore.serverFile.attributes.url.attributes.url} alt="" />
+               </dd>
+               <dt>更多尺寸</dt>
+               <dd>...</dd>
+             </dl>
+          </Result>)
           : ''}
       </div>
     </>
