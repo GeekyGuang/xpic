d90af56c7e2041638cbef485e42bf8efa5532a9a
TravisWongX <travis.x.wong@gmail.com>
2022-03-27 16:28:08 +0800

    实现点击按钮复制文本

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,9 +1,10 @@
 import { useStore } from '../store'
-import { Upload } from 'antd'
+import { Button, message, Upload } from 'antd'
 import { InboxOutlined } from '@ant-design/icons'
-import { observer } from 'mobx-react'
+import { observer, useLocalStore } from 'mobx-react'
 import styled from 'styled-components'
 import Loading from './Loading'
+import { MutableRefObject, useRef } from 'react'
 
 const { Dragger } = Upload
 
@@ -27,9 +28,10 @@ const Result = styled.div`
 
     > dt {
       padding-bottom: 6px;
+      font-weight: 600;
     }
 
-    a {
+    /* a {
       display: inline-block;
       width: 100%;
       border: 1px solid #d9d9d9;
@@ -37,18 +39,37 @@ const Result = styled.div`
       white-space: wrap;
       word-wrap: break-word;
       background: white;
-    }
+    } */
 
     img {
       max-height: 280px;
       max-width: 100%;
       object-fit: contain;
     }
+
+    .resize {
+      > input {
+        width: 100px;
+        border: 1px solid #d9d9d9;
+
+        &:focus {
+          outline: none;
+        }
+      }
+    }
   }
 `
 
 const Uploader = observer(() => {
   const { ImageStore } = useStore()
+  const onlineUrlRef = useRef() as MutableRefObject<HTMLInputElement>
+  const resizeUrlRef = useRef() as MutableRefObject<HTMLInputElement>
+
+  const handleCopy = (ref) => {
+    ref.select()
+    document.execCommand('copy')
+    message.success('复制成功')
+  }
 
   const props = {
     showUploadList: false,
@@ -97,13 +118,29 @@ const Uploader = observer(() => {
             <dl>
               <dt>线上地址</dt>
               <dd>
-                <a
-                  target="_blank"
-                  href={ImageStore.serverFile.attributes.url.attributes.url}
-                  rel="noreferrer"
+                <input
+                  type="text"
+                  className="filename"
+                  value={ImageStore.serverFile.attributes.url.attributes.url}
+                  ref={onlineUrlRef}
+                  readOnly
+                />
+                <Button
+                  type="default"
+                  size="small"
+                  onClick={() => handleCopy(onlineUrlRef.current)}
                 >
-                  {ImageStore.serverFile.attributes.url.attributes.url}
-                </a>
+                  复制
+                </Button>
+                <Button type="default" size="small">
+                  <a
+                    target="_blank"
+                    href={ImageStore.serverFile.attributes.url.attributes.url}
+                    rel="noreferrer"
+                  >
+                    打开
+                  </a>
+                </Button>
               </dd>
               <dt>文件名</dt>
               <dd>
@@ -122,7 +159,35 @@ const Uploader = observer(() => {
                 />
               </dd>
               <dt>更多尺寸</dt>
-              <dd>...</dd>
+              <dd className="resize">
+                宽 <input type="text" /> 高 <input type="text" />
+              </dd>
+              <dd>
+                <input
+                  type="text"
+                  className="filename"
+                  readOnly
+                  ref={resizeUrlRef}
+                />
+                <Button
+                  type="default"
+                  size="small"
+                  onClick={() => {
+                    handleCopy(resizeUrlRef.current)
+                  }}
+                >
+                  复制
+                </Button>
+                <Button type="default" size="small">
+                  <a
+                    target="_blank"
+                    href={ImageStore.serverFile.attributes.url.attributes.url}
+                    rel="noreferrer"
+                  >
+                    打开
+                  </a>
+                </Button>
+              </dd>
             </dl>
           </Result>
         ) : (
